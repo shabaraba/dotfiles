d3a3cd23a65c48ccd76803a7638fadab01b7f8ea
shabaraba <work.shabaraba@gmail.com>
2022-01-19 13:22:47 +0900

    make installer shell files

--- installers/neovim_installer.sh
+++ installers/neovim_installer.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/sh
 
-if [ $1 = 'ubuntu' ]; then
+if [ "$1" = ubuntu ]; then
     echo 'ubuntu' 1>&2
     install_cmd='apt install -y'
-elif [ $1 = 'centos' ]; then
+    apt update -y && apt upgrade -y
+elif [ "$1" = centos ]; then
     echo 'centos' 1>&2
     install_cmd='yum install -y'
+    yum update -y
 else
     echo 'macos' 1>&2
     install_cmd='brew install -y'
@@ -13,11 +15,14 @@ fi
 
 if !(type "nvim" > /dev/null 2>&1); then
     pip3 install neovim && pip3 install pynvim
-    git clone https://github.com/neovim/neovim.git
-    $install_cmd libtool automake cmake libncurses5-dev g++ gettext
+    if [ ! -e ./neovim ]; then
+        git clone https://github.com/neovim/neovim.git
+    fi
+    $install_cmd pkg-config libtool-bin libtool automake cmake libncurses5-dev g++ gettext
     cd neovim
     make CMAKE_BUILD_TYPE=RelWithDebInfo
     make install;
+    cd ../ && rm -rf neovim
 else
     echo 'neovim already installed, skip.'
 fi
--- installers/zsh_installer.sh
+++ installers/zsh_installer.sh
@@ -1,21 +1,24 @@
 #! /usr/bin/sh
 
-if [ $1 = 'ubuntu' ]; then
+if [ $1='ubuntu' ]; then
     echo 'ubuntu' 1>&2
     install_cmd='apt install -y'
-elif [ $1 = 'centos' ]; then
+    apt update -y && apt upgrade -y
+elif [ $1='centos' ]; then
     echo 'centos' 1>&2
     install_cmd='yum install -y'
+    yum update -y
 else
     echo 'macos' 1>&2
     install_cmd='brew install -y'
 fi
 
 if !(type "zsh" > /dev/null 2>&1); then
-    curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+    $install_cmd zsh
 else
     echo 'zsh already installed, skip.'
 fi
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 exit 1
 
