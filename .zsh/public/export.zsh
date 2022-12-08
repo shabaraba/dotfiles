7030a2f076a5b2f74d95110880132181761fe7a1
shabaraba <work.shabaraba@gmail.com>
2022-12-08 12:57:22 +0900

    - divide .zshrc files

---
+++ .zsh/public/export.zsh
@@ -0,0 +1,57 @@
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+export PATH=/usr/bin/node:$PATH
+typeset -U path PATH
+path=(
+  /opt/homebrew/bin(N-/)
+  /opt/homebrew/sbin(N-/)
+  /usr/bin
+  /usr/sbin
+  /bin
+  /sbin
+  /usr/local/bin(N-/)
+  /usr/local/sbin(N-/)
+  /Library/Apple/usr/bin
+)
+
+export PATH="/usr/local/opt/ncurses/bin:$PATH"
+export PATH="/usr/local/opt/ncurses/bin:$PATH"
+
+export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="$PATH:/usr/local/opt/mysql-client@5.7/bin"
+
+# nodenv
+[[ -d ~/.nodenv  ]] && \
+    export PATH="$HOME/.nodenv/bin:$PATH" && \
+    eval "$(nodenv init -)"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+
+export ANDROID_SDK_ROOT=$HOME/.android
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/tools/bin:$PATH
+export PATH=$ANDROID_SDK_ROOT/cmdline-tools/tools/lib:$PATH
+export PATH=$ANDROID_SDK_ROOT/platform-tools:$PATH
+export PATH=$ANDROID_SDK_ROOT/emulator:$PATH
+
+[[ -e ~/.phpbrew/bashrc ]] && source ~/.phpbrew/bashrc
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# export FLUTTER_ROOT=$HOME/Flutter/Sdk
+# export PATH=$FLUTTER_ROOT/bin:$PATH
+
+# phpenv
+export PATH="$HOME/.phpenv/bin:$PATH"
+eval "$(phpenv init -)"
+export PATH="$HOME/.phpenv/bin:$PATH"
+eval "$(phpenv init -)"
+
---
+++ .zsh/public/setopt.zsh
@@ -0,0 +1,16 @@
+setopt print_eight_bit
+setopt auto_cd
+setopt no_beep
+setopt nolistbeep
+setopt auto_pushd
+ 
+setopt share_history
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt hist_reduce_blanks
+setopt correct
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt extended_glob
+
