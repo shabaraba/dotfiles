a6be6d54d9364cb45423ae03f39ad24c91f5bc72
shabaraba <work.shabaraba@gmail.com>
2024-05-07 16:59:13 +0900

    set lazy load for some plugins

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -72,27 +72,6 @@ M.dashboard = function()
   map("n", "<leader>ws", ":SessionSave <CR>")
 end
 
-M.lspconfig = function()
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
-  map("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>")
-  map("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<CR>")
-  map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
-  map("n", "gk", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
-  map("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>")
-  map("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>")
-  map("n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>")
-  map("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
-  map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
-  map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
-  map("n", "<C-]><C-]>", "<cmd>lua vim.lsp.buf.references()<CR>")
-  map("n", "ge", "<cmd>lua vim.diagnostic.open_float()<CR>")
-  map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
-  map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>")
-  map("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
-  map("n", "<leader>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>")
-end
-
 M.neotree = function()
   map("n", "<leader>b", ":Neotree toggle <CR>")
   map("n", "<leader>bg", ":Neotree git_status toggle <CR>")
