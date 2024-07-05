04120a98d09d7df634a57151cbacc8b122a09626
shabaraba <work.shabaraba@gmail.com>
2024-07-05 09:48:01 +0900

    add shortcut for workspace

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -1,4 +1,6 @@
 local wezterm = require'wezterm';
+local config = wezterm.config_builder();
+local act = require "wezterm".action;
 
 local BinaryFormat = package.cpath:match("%p[\\|/]?%p(%a+)")
 if BinaryFormat == "dll" then
@@ -24,7 +26,14 @@ if BinaryFormat == 'Windows' then
     decorations_ = 'RESIZE';
 end
 
-return {
+wezterm.on('update-status', function(window, pane)
+  local workspace = window:active_workspace()
+  window:set_left_status(wezterm.format({
+    {Text = ' ' .. workspace .. ' '}
+  }))
+end)
+
+config = {
     initial_rows = 30,
     initial_cols = 150,
     window_decorations = decorations_;
@@ -47,10 +56,55 @@ return {
     harfbuzz_features = {"calt=1", "clig=1", "liga=1"},
     exit_behavior = "Close",
 
+    leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
     keys = {
         {key = "w", mods = "ALT", action = wezterm.action{CloseCurrentPane={confirm=true}}},
         {key = 'h', mods = 'CMD', action = wezterm.action.Hide},
+        -- Create new workspace
+        {key = 'W', mods = 'LEADER|SHIFT', action = act.PromptInputLine {
+            description = "(wezterm) Create new workspace:",
+            action = wezterm.action_callback(function(window, pane, line)
+              if line then
+                window:perform_action(
+                  act.SwitchToWorkspace {
+                    name = line,
+                  },
+                  pane
+                )
+              end
+            end
+          )},
+        },
+        -- Switch workspace
+        {key = 'w', mods = 'LEADER', action = wezterm.action_callback(
+          function (win, pane)
+            -- workspace のリストを作成
+            local workspaces = {}
+            for i, name in ipairs(wezterm.mux.get_workspace_names()) do
+              table.insert(workspaces, {
+                id = name,
+                label = string.format("%d. %s", i, name),
+              })
+            end
+            local current = wezterm.mux.get_active_workspace()
+            -- 選択メニューを起動
+            win:perform_action(act.InputSelector {
+              action = wezterm.action_callback(function (_, _, id, label)
+                if not id and not label then
+                  wezterm.log_info "Workspace selection canceled"  -- 入力が空ならキャンセル
+                else
+                  win:perform_action(act.SwitchToWorkspace { name = id }, pane)  -- workspace を移動
+                end
+              end),
+              title = "Select workspace",
+              choices = workspaces,
+              fuzzy = true,
+              -- fuzzy_description = string.format("Select workspace: %s -> ", current), -- requires nightly build
+            }, pane)
+          end
+        )},
     },
     default_prog = default_prog_,
 }
 
+return config;
