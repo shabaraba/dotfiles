9037c7d077b787fa7e0c89b28bf13c4b23e7449e
shabaraba <work.shabaraba@gmail.com>
2024-08-29 22:32:03 +0900

    feat(wezterm): update window visual

--- terminal/wezterm/wezterm.lua
+++ terminal/wezterm/wezterm.lua
@@ -19,11 +19,9 @@ end
 BinaryFormat = nil
 
 local default_prog_ = nil;
-local decorations_ = 'TITLE | RESIZE';
 
 if BinaryFormat == 'Windows' then
     default_prog_ = {"wsl.exe", "--exec", "/bin/zsh", "-l"};
-    decorations_ = 'RESIZE';
 end
 
 wezterm.on('update-status', function(window, pane)
@@ -36,7 +34,33 @@ end)
 config = {
     initial_rows = 30,
     initial_cols = 150,
-    window_decorations = decorations_;
+    window_decorations = 'RESIZE',
+    window_frame = {
+      -- inactive_titlebar_bg = '#353535',
+      -- active_titlebar_bg = '#2b2042',
+      -- inactive_titlebar_fg = '#cccccc',
+      -- active_titlebar_fg = '#ffffff',
+      -- inactive_titlebar_border_bottom = '#2b2042',
+      -- active_titlebar_border_bottom = '#2b2042',
+      -- button_fg = '#cccccc',
+      -- button_bg = '#2b2042',
+      -- button_hover_fg = '#ffffff',
+      -- button_hover_bg = '#3b3052',
+      -- border_left_width = '0.5cell',
+      -- border_right_width = '0.5cell',
+      -- border_bottom_height = '0.25cell',
+      -- border_top_height = '0.25cell',
+      -- border_left_color = 'purple',
+      -- border_right_color = 'purple',
+      -- border_bottom_color = 'purple',
+      -- border_top_color = '#ffffff',
+    },
+    window_padding = {
+      left = 0,
+      right = 0,
+      top = 0,
+      bottom = 0
+    },
     window_background_image_hsb = {
         -- Darken the background image by reducing it to 1/3rd
         brightness = 0.3,
