607b43622d77bdabfae74fcd7fdbdb68b06bcd2f
shabaraba <work.shabaraba@gmail.com>
2024-08-29 22:25:43 +0900

    chore(nvim): add pyenv and its environment dir

--- nvim/lua/root.lua
+++ nvim/lua/root.lua
@@ -10,6 +10,8 @@ for _, module in ipairs(core_modules) do
    end
 end
 
+vim.g.python3_host_prog = vim.fn.expand('~/.config/nvim/env/bin/python')
+
 require("core")
 require("plugins")
 -- non plugin mappings
