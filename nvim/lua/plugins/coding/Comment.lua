b7f50dfa80db74625401acb67752f6379770c82f
shabaraba <work.shabaraba@gmail.com>
2025-02-18 15:46:04 +0900

    feat(nvim): comment out for mqh

--- nvim/lua/plugins/coding/Comment.lua
+++ nvim/lua/plugins/coding/Comment.lua
@@ -1,5 +1,18 @@
 return {
   "numToStr/Comment.nvim",
   keys = require("mappings").comment,
-  opts = {},
+  opts = {
+      pre_hook = function(ctx)
+    local filetype = vim.bo.filetype
+    if filetype == 'mq5' then
+      return require('Comment.ft').set('mq5', {'// %s', '/* %s */'})
+    end
+  end
+  },
+  config = function ()
+    local ft = require('Comment.ft')
+   ft.set('mq5', {'//%s', '/*%s*/'})
+   ft.set('mql', {'//%s', '/*%s*/'})
+   .set('mqh', {'//%s', '/*%s*/'})
+  end
 }
