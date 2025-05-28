65c23a83db01b0913313cc317a4b89d9212a53e6
shabaraba <work.shabaraba@gmail.com>
2025-05-28 17:05:58 +0900

    fix: for claud

--- terminal/wezterm/settings/font.lua
+++ terminal/wezterm/settings/font.lua
@@ -4,14 +4,30 @@ local M = {}
 
 function M.get_config()
   return {
-    font_dirs = { wezterm.config_dir.."/fonts" },
+    font_dirs = { wezterm.config_dir .. "/fonts" },
     font_size = 14,
     font = wezterm.font_with_fallback {
       "HackGenNerd Console",
       "Noto Sans CJK JP",
     },
     harfbuzz_features = { "calt=1", "clig=1", "liga=1" },
+    font_rules = {
+      {
+        intensity = 'Normal',
+        italic = true,
+        font = wezterm.font_with_fallback {
+          {
+            family = "HackGenNerd Console",
+            -- 斜体を合成させない
+            style = "Normal",
+            synthesis = "None",
+          },
+          "Noto Sans CJK JP",
+        },
+      },
+    }
   }
 end
 
 return M
+
