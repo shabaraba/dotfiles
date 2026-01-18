e7241ca68a81af227df3a4114d0204608e393e6b
shabaraba <work.shabaraba@gmail.com>
2026-01-19 00:03:47 +0900

    fix: fix mise and change ime-auto settings for latest

--- nvim/lua/plugins/coding/ime-auto.lua
+++ nvim/lua/plugins/coding/ime-auto.lua
@@ -1,14 +1,10 @@
 return {
   "shabaraba/ime-auto.nvim",
-  dev = true,  -- ~/workspaces/nvim-plugins/ime-auto.nvim を使用
+  dev = true,
   event = { "InsertEnter", "CmdlineEnter" },
   config = function()
     require("ime-auto").setup({
       escape_sequence = "ｋｊ",
-      escape_timeout = 200,
-      os = "auto",
-      ime_method = "builtin",
-      debug = false,
     })
   end,
 }
