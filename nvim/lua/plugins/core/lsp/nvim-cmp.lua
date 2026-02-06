09906c8ae78ce1f0e7f7a990b8b7f53c3bcbde13
shabaraba <work.shabaraba@gmail.com>
2026-02-06 15:04:12 +0900

    fix(nvim,ghostty): resolve Ctrl+L keybinding conflict for Copilot
    - Remove Ctrl+L mapping from nvim-cmp to avoid conflict with Copilot
    - Add ctrl+l=unbind in Ghostty config to passthrough to Neovim
    - Fixes issue where Copilot suggestion accept (Ctrl+L) was not working in Ghostty terminal

--- nvim/lua/plugins/core/lsp/nvim-cmp.lua
+++ nvim/lua/plugins/core/lsp/nvim-cmp.lua
@@ -66,7 +66,6 @@ return {
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-l>"] = cmp.mapping.confirm({ select = true }),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
       }),
       -- sources for autocompletion
