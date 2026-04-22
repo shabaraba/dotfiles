fad9ef0f6a3b9d872ba39773fbff2b85988a56ec
shabaraba <work.shabaraba@gmail.com>
2026-04-22 09:17:12 +0900

    feat: add open new wezterm window script by raycast

---
+++ terminal/wezterm/open-new-wezterm-window.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open New Wezterm Window
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🖥️
+
+wezterm cli spawn --new-window
