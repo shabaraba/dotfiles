da689f4492e091277e7077fa6c1009faa0fd2e85
shabaraba <work.shabaraba@gmail.com>
2026-05-26 10:46:37 +0900

    fix: remain files added .gitignore

--- sh/zsh/contexts/abbreviations.zsh
+++ sh/zsh/contexts/abbreviations.zsh
@@ -46,7 +46,7 @@ abbr "gsl"="git stash list"
 abbr "gsp"="git stash pop"
 abbr "gst"="git stash"
 abbr "gsu"="git stash -u"
-abbr "gpuld"='git fetch --depth 1 origin $(git branch --show-current) && git reset --hard origin/$(git branch --show-current) && git clean -fdx && git gc --prune=now --quiet'
+abbr "gpuld"='git fetch --depth 1 origin $(git branch --show-current) && git reset --hard origin/$(git branch --show-current) && git clean -fd && git gc --prune=now --quiet'
 abbr "gsm"="git-shallow-merge"
 
 abbr "poff"="http_proxy= https_proxy="
