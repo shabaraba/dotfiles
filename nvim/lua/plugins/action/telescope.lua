389ad81aef88108bbf9c5a019210b6e8910e6d0b
shabaraba <work.shabaraba@gmail.com>
2026-06-26 16:07:08 +0900

    fix: telescopeの表示長さを変更

--- nvim/lua/plugins/action/telescope.lua
+++ nvim/lua/plugins/action/telescope.lua
@@ -41,7 +41,7 @@ return {
       -- file_sorter = require("telescope.sorters").get_fuzzy_file,
       file_ignore_patterns = { "node_modules" },
       -- generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-      path_display = { "truncate" },
+      path_display = { "smart" }, --, shorten = { len = 5, exclude = { -1 } } },
       winblend = 0,
       border = {},
       borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
